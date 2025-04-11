90673029d0820ad96d677efba1ad26648c2261b8
humorslyrautela <abhishekrautela099@gmail.com>
2025-04-11 18:40:07 +0530

    created basic header section

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
-
+import { HiSearch, HiBell, HiChat } from 'react-icons/hi'
 function Header() {
   return (
     <div>
@@ -9,6 +9,14 @@ function Header() {
 
          <button>Home</button>
          <button>Create</button>
+         <div>
+          <HiSearch/>
+          <input type="text" placeholder='Search'/>
+          <HiBell/>
+          <HiChat/>
+        <Image src ="/man.png" alt='user-image' width={50} height={50} className='hover:bg-gray-300 p-2
+        rounded-full cursor-pointer'/>
+         </div>
         
     </div>
   )
